21741d3fc0217bc476a3c7ec59b56318b9df247f
dominz2 <dominik32110@gmail.com>
2021-10-24 17:31:43 +0200

    Function create table was added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
+/* For given two-dimensional M x N table shift all its
+elements in spiral direction */
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+
+/*
+in function "create_table by using vector
+template object "table" is created
+*/
+vector<vector<int>> create_table()
+{
+    int m, n;
+    cout << "Enter M i N" <<endl;
+    cin >> m;
+    cin >> n;
+    vector<vector<int>> table(m, vector<int>(n));
+    return table;
+}
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<vector<int>> table;
+    table = create_table();
+    cout<<table[0][0]<<endl;
     return 0;
 }
