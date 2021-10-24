7be406926518b7263806169232c7e4525fe63468
dominz2 <dominik32110@gmail.com>
2021-10-24 18:38:41 +0200

    function fill_table and print_table was added

--- main.cpp
+++ main.cpp
@@ -21,12 +21,39 @@ vector<vector<int>> create_table()
     return table;
 }
 
+vector<vector<int>> fill_table(vector<vector<int>> table)
+{
+    int i, j;
+    for (j=0; j<table.size(); j++)
+    {
+        cout<<"enter elements: " << j <<"-th row"<<endl;
+        for (i=0; i<table[j].size(); i++)
+        {
+            cin>>table[j][i];
+        }
+    }
+return table;
+}
+
+void print_table(vector<vector<int>> table)
+{
+    int i, j;
+    for (j=0; j<table.size(); j++)
+    {   cout<<endl;
+        for (i=0; i<table[j].size(); i++)
+        {
+            cout<<table[j][i];
+            cout<<" ";
+        }
+    }
+}
 
 
 int main()
 {
     vector<vector<int>> table;
     table = create_table();
-    cout<<table[0][0]<<endl;
+    table = fill_table(table);
+    print_table(table);
     return 0;
 }
