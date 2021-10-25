d7688bdbe7cf7ea2f74351126cd19d4b80e4d430
dominz2 <dominik32110@gmail.com>
2021-10-25 13:04:33 +0200

    Function create_table was reinforced Passing arguments to functions was changed to reference Function spiral_spin was added

--- main.cpp
+++ main.cpp
@@ -11,17 +11,28 @@ using namespace std;
 in function "create_table by using vector
 template object "table" is created
 */
-vector<vector<int>> create_table()
+
+
+void create_table(vector<vector<int>> &table)
 {
-    int m, n;
+    int m=0, n=0;
     cout << "Enter M i N" <<endl;
+    while (m==0 || n==0)
+    {
     cin >> m;
     cin >> n;
-    vector<vector<int>> table(m, vector<int>(n));
-    return table;
+    if (m==0 || n==0)
+        {
+            cout<<"Number of rows and columns have to be positive"<<endl;
+        }
+    }
+
+    vector<vector<int>> table1(m, vector<int>(n));
+    table = table1;
 }
 
-vector<vector<int>> fill_table(vector<vector<int>> table)
+
+void fill_table(vector<vector<int>> &table)
 {
     int i, j;
     for (j=0; j<table.size(); j++)
@@ -32,8 +43,8 @@ vector<vector<int>> fill_table(vector<vector<int>> table)
             cin>>table[j][i];
         }
     }
-return table;
 }
+
 
 void print_table(vector<vector<int>> table)
 {
@@ -49,11 +60,70 @@ void print_table(vector<vector<int>> table)
 }
 
 
+void spiral_spin(vector<vector<int>> &table)
+{
+    int top = 0, bottom = table.size() - 1;
+    int left = 0, right = table[0].size() - 1;
+    int prev = table[0][0];
+
+    while (true)
+    {
+
+        // change top row
+        for (int i = left; i <= right; i++) {
+            swap(table[top][i], prev);
+        }
+
+        top++;
+
+        if (top > bottom) {
+            break;
+        }
+
+        // change right column
+        for (int i = top; i <= bottom; i++) {
+            swap(table[i][right], prev);
+        }
+
+        right--;
+
+        if (left > right) {
+            break;
+        }
+
+        // change bottom row
+        for (int i = right; i >= left; i--) {
+            swap(table[bottom][i], prev);
+        }
+
+        bottom--;
+
+        if (top > bottom) {
+            break;
+        }
+
+        // change left column
+        for (int i = bottom; i >= top; i--) {
+            swap(table[i][left], prev);
+        }
+
+        left++;
+    }
+
+    // the first element of the matrix will be the last element replaced
+    table[0][0] = prev;
+}
+
+
 int main()
 {
     vector<vector<int>> table;
-    table = create_table();
-    table = fill_table(table);
+    create_table(table);
+    fill_table(table);
+    print_table(table);
+    spiral_spin(table);
+    cout<<endl;
+    cout<<"Table after spin";
     print_table(table);
     return 0;
 }
