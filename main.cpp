f0273e410f58bb4abba715ee9b8fb549e3d13609
dominz2 <dominik32110@gmail.com>
2021-10-31 18:02:20 +0100

    Several function was added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ elements in spiral direction */
 #include <vector>
 #include <chrono>
 #include <fstream>
+#include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -14,38 +16,115 @@ in function "create_table by using vector
 template object "table" is created
 */
 
-
-void create_table(vector<vector<int>> &table)
+vector<vector<int>> create_table()
 {
     int m=0, n=0;
     cout << "Enter M i N" <<endl;
-    while (m==0 || n==0)
+    while (true)
     {
     cin >> m;
     cin >> n;
-    if (m==0 || n==0)
-        {
-            cout<<"Number of rows and columns have to be positive"<<endl;
-        }
+    if (m > 0 && n > 0)
+        break;
+    cout<<"Number of rows and columns have to be positive"<<endl;
     }
-
-    vector<vector<int>> table1(m, vector<int>(n));
-    table = table1;
+    vector<vector<int>> table(m, vector<int>(n));
+    return table;
 }
 
+vector<vector<int>> read_table_from_txt()
+{   string table_txt = "table.txt";
+    ifstream in (table_txt);
+    string line;
+
+    int line_count = 0;
+    while(getline(in, line))
+        line_count++;
+
+    in.clear();
+    in.seekg(0);
+
+    vector<vector<int>> table(line_count, vector<int>(0));
+    int buffor;
+    int i = 0;
+
+    while(getline(in, line))
+    {
+        stringstream ss(line);
+
+            while(ss >> buffor)
+            table[i].push_back(buffor);
+        i++;
+    }
+    return table;
+
+}
 
 void fill_table(vector<vector<int>> &table)
 {
     int i, j;
-    for (j=0; j<table.size(); j++)
+    cout << "Do you prefer to fill table manually or randomly?" <<endl;
+    cout << "Press 1 for manual option or 2 for automatic option" <<endl;
+    int choice;
+    cin >> choice;
+    switch (choice)
     {
-        cout<<"enter elements: " << j <<"-th row"<<endl;
-        for (i=0; i<table[j].size(); i++)
-        {
-            cin>>table[j][i];
-        }
+        case 1:
+            {
+            for (j=0; j<table.size(); j++)
+            {
+                cout<<"enter elements: " << j <<"-th row"<<endl;
+                for (i=0; i<table[j].size(); i++)
+                {
+                    cin>>table[j][i];
+                }
+            }
+            break;
+            }
+
+
+        case 2:
+            {
+            srand((unsigned) time(0));
+            for (j=0; j<table.size(); j++)
+            {
+                for (i=0; i<table[j].size(); i++)
+                {
+                table[j][i] = rand();
+                }
+            }
+            break;
+            }
+
+
+    }
+
+
+}
+
+void initialize_table(vector<vector<int>> &table)
+{
+    cout << "Do you want to create table or read it from a txt file?" << endl;
+    cout << "Press 1 for create or 2 for read" << endl;
+    int choice;
+    cin >> choice;
+    switch(choice)
+    {
+    case 1:
+        table = create_table();
+        fill_table(table);
+        break;
+
+    case 2:
+        table = read_table_from_txt();
+        break;
     }
 }
+
+
+
+
+
 
 
 void print_table(vector<vector<int>> table)
@@ -116,6 +195,8 @@ void spiral_spin(vector<vector<int>> &table)
     table[0][0] = prev;
 }
 
+//chcemy, ¿eby mo¿na by³o równie¿ wczytywaæ zapisany ju¿ txt do vector<vector<int>>
+
 void save_table_to_txt (vector<vector<int>> &table)
 {
     int i, j;
@@ -135,18 +216,19 @@ void save_table_to_txt (vector<vector<int>> &table)
 
 
 
+
 int main()
 {
 
     vector<vector<int>> table;
-    create_table(table);
-    fill_table(table);
+    initialize_table(table);
     print_table(table);
     spiral_spin(table);
     cout<<endl;
     cout<<"Table after spin";
     print_table(table);
     save_table_to_txt(table);
+
 
     return 0;
 }
